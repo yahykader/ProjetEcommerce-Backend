27f94d981a365f45239c49d7065d340fe0a080fd
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2019-09-26 12:13:23 +0200

    add uploadFile

--- src/main/java/org/greta/web/ProduitController.java
+++ src/main/java/org/greta/web/ProduitController.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 public class ProduitController {
@@ -26,8 +28,15 @@ public class ProduitController {
 		 Produit p= produitRepository.findById(idProduit).get();
 		 System.out.println(p);
 		 return Files.readAllBytes(Paths.get(System.getProperty("user.home")+"/ecom/produits/"+p.getPhoto()));
-		
-	
+	}
+
+	@PostMapping(path="/uploadPhoto/{idProduit}")
+	public void uploadPhoto(MultipartFile file,@PathVariable("idProduit") Long idProduit) throws  Exception{
+            Produit p=produitRepository.findById((idProduit)).get();
+            System.out.println(p);
+            p.setPhoto(idProduit+".png");
+            Files.write(Paths.get(System.getProperty("user.home")+"/ecom/produits/"+p.getPhoto()),file.getBytes());
+            produitRepository.save(p);
 	}
 	
 
